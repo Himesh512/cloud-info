1b04ccfc7cc8b50bda9e433ea23e3fe447e12915
HimsM5 <himesh.aadeshara@marutitech.com>
2024-01-02 12:00:42 +0530

    footer changes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const mobileNav = document.querySelector(".mobile-navbar-btn");
 const navHeader = document.querySelector(".header");
+const navAnchors = document.querySelectorAll(".navbar-list a")
 
 const toggleNavBar = () => {
     navHeader.classList.toggle("active");
 }
-
 mobileNav.addEventListener("click", () => toggleNavBar());
+
+navAnchors.forEach((aEl) => {
+    aEl.addEventListener("click", () => toggleNavBar());
+});
